c5d58231b56af7abb7e9641f332ac247d7469909
lu <1625196521@qq.com>
2020-11-08 17:06:07 +0800

    s3

---
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.capability.demospringioccontainer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Foo {
+    private Bar bar;
+
+    public void hi() {
+        System.out.println("Hi, " + bar.name());
+    }
+
+    public String name() {
+        return "Foo";
+    }
+}
