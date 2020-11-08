b56e293c8f92409760713a846e2ecaf5d139e1b7
lu <1625196521@qq.com>
2020-11-08 17:13:41 +0800

    s3

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,10 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bar {
     private Foo foo;
+
+    @Autowired
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
 
     public void hi() {
         System.out.println("Hi, " + foo.name());
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,10 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Foo {
     private Bar bar;
+
+    @Autowired
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
